54bec2c4756bdf9f6ecb5969f7ab54661522c21e
Ernst <ernstchristia@gmail.com>
2025-07-26 11:24:22 +0800

    update AddPageButton

--- components/AddPageButton.tsx
+++ components/AddPageButton.tsx
@@ -1,8 +1,10 @@
+import { Plus } from "lucide-react";
 export const AddPageButton = ({ onClick }: { onClick: () => void }) => (
   <button
     onClick={onClick}
     className="px-4 py-2 rounded-md bg-white text-sm border hover:shadow"
   >
-    + Add page
+    <Plus className="w-5 h-5 mr-1 inline-block" />
+    <span>Add Page</span>
   </button>
 );
